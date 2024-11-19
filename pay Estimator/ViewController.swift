a0e46334a45a369a7396057ef67326b8425cf7cf
Dallas Wheatley <dallasdevco@gmail.com>
2024-11-19 16:50:53 +0800

    Initial Commit

---
+++ pay Estimator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pay Estimator
+//
+//  Created by Dallas Wheatley on 19/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pay EstimatorTests/pay_EstimatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  pay_EstimatorTests.swift
+//  pay EstimatorTests
+//
+//  Created by Dallas Wheatley on 19/11/2024.
+//
+
+import Testing
+@testable import pay_Estimator
+
+struct pay_EstimatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
